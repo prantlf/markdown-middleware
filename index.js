dcb6ebbc0d2238f31d84b08fbb7a081d8b2d1a86
Jake Rosoman <jkroso@gmail.com>
2014-04-16 18:15:02 +1200

    tabs -> spaces

--- index.js
+++ index.js
@@ -15,76 +15,76 @@ var age = Date.now() - stat.mtime
 // update css if old
 
 if (age > ms('5 days')) {
-	get('https://github.com/aheckmann/greadme', function(e, body){
-		if (e) throw e
-		var r = /href=["']([^"']+\.css)/g
-		var batch = new Batch
-		var m
-		while (m = r.exec(body)) add(m[1])
-		function add(url){
-			batch.push(function(done){ get(url, done) })
-		}
-		batch.end(function(e, files){
-			if (e) throw e
-			rmdir(__dirname + '/css')
-			fs.mkdirSync(__dirname + '/css')
-			files.forEach(function(css, i){
-				var file = __dirname + '/css/' + i + '.css'
-				fs.writeFileSync(file, css)
-			})
-		})
-	})
+  get('https://github.com/aheckmann/greadme', function(e, body){
+    if (e) throw e
+    var r = /href=["']([^"']+\.css)/g
+    var batch = new Batch
+    var m
+    while (m = r.exec(body)) add(m[1])
+    function add(url){
+      batch.push(function(done){ get(url, done) })
+    }
+    batch.end(function(e, files){
+      if (e) throw e
+      rmdir(__dirname + '/css')
+      fs.mkdirSync(__dirname + '/css')
+      files.forEach(function(css, i){
+        var file = __dirname + '/css/' + i + '.css'
+        fs.writeFileSync(file, css)
+      })
+    })
+  })
 }
 
 module.exports = function(opts){
-	var dir = path.resolve(opts.directory)
-	var cssDir = __dirname + '/css'
+  var dir = path.resolve(opts.directory)
+  var cssDir = __dirname + '/css'
 
-	return function(req, res, next) {
-		var file = req.url
+  return function(req, res, next) {
+    var file = req.url
 
-		if (!(/\.m(?:d|arkdown)$/i).test(file)) return next()
+    if (!(/\.m(?:d|arkdown)$/i).test(file)) return next()
 
-		file = path.join(dir, file)
+    file = path.join(dir, file)
 
-		fs.readFile(file, 'utf8', function(e, md){
-			if (e) return next(e)
-			var template = fs.readFileSync(__dirname + '/index.html', 'utf8')
-			compile(md, {
-				gfm: true,
-				pedantic: false,
-				tables: true,
-				breaks: false,
-				sanitize: false,
-				smartLists: true,
-				smartypants: false,
-				highlight: function(code, lang, done){
-					highlight({lang: lang, format: 'html'}, code, done)
-				}
-			}, function(e, html){
-				res.end(ejs.render(template, {
-					css: fs.readdirSync(cssDir).map(function(name){
-						return fs.readFileSync(cssDir + '/' + name, 'utf8')
-					}),
-					markdown: html,
-					title: path.relative(dir, file)
-				}))
-			})
-		})
-	}
+    fs.readFile(file, 'utf8', function(e, md){
+      if (e) return next(e)
+      var template = fs.readFileSync(__dirname + '/index.html', 'utf8')
+      compile(md, {
+        gfm: true,
+        pedantic: false,
+        tables: true,
+        breaks: false,
+        sanitize: false,
+        smartLists: true,
+        smartypants: false,
+        highlight: function(code, lang, done){
+          highlight({lang: lang, format: 'html'}, code, done)
+        }
+      }, function(e, html){
+        res.end(ejs.render(template, {
+          css: fs.readdirSync(cssDir).map(function(name){
+            return fs.readFileSync(cssDir + '/' + name, 'utf8')
+          }),
+          markdown: html,
+          title: path.relative(dir, file)
+        }))
+      })
+    })
+  }
 }
 
 function buffer(stream, cb){
-	var buf = ''
-	stream.on('readable', function(){
-		buf += this.read() || ''
-	}).on('end', function(){
-		cb(null, buf)
-	}).on('error', cb)
+  var buf = ''
+  stream.on('readable', function(){
+    buf += this.read() || ''
+  }).on('end', function(){
+    cb(null, buf)
+  }).on('error', cb)
 }
 
 function get(url, cb){
-	https.get(url, function(res){
-		buffer(res, cb)
-	})
+  https.get(url, function(res){
+    buffer(res, cb)
+  })
 }
